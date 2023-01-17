16f08ab644f49eadb09e2946f09d22a633b8175b
Göktuğ GÖK <gokgoktug0@gmail.com>
2023-01-17 13:07:19 +0300

    website

---
+++ dist/js/main.js
@@ -0,0 +1,9 @@
+mediumZoom('.zoom',{
+    margin: 55,
+    scrollOffset: 200,
+    background: '#fff',
+});
+
+ScrollReveal().reveal('.right',{ duration: 2000 ,distance:"60px",origin:"right"});
+ScrollReveal().reveal('.left',{ duration: 2000 ,distance:"60px",origin:"left"});
+ScrollReveal().reveal('.top',{ duration: 2000 ,distance:"-50px",origin: "top",interval:500 });
